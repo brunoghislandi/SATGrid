3277a96af6bb5eaec424479e6e9b63c28f67c5ff
valentim.koniarski <valentim.koniarski@hotmail.com>
2021-11-21 22:00:57 -0300

    EditSemester

--- screens/src/EditSemester.js
+++ screens/src/EditSemester.js
@@ -1,35 +1,111 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { Button, Text, TextInput, Switch } from 'react-native-paper';
 
-export default function EditSemester() {
+import { Picker } from "@react-native-picker/picker";
 
-    const returnText = 'o cara se chama felipekkkkkkkkkkkkkkkk'
-    const [showIt, setShowIt] = useState(false);
+import subjects from './mocks/subjects.json';
+
+
+function SelectSubjects({ subjectsOfWeek, dayOfWeek }) {
+
+    const [selectedSubject, setSelectedSubject] = useState();
+
 
     return (
-        <View style={styles.basic}>
-            <Text>muitos testes</Text>
-            <Button style={styles.button} mode="contained" color="yellow"
-                onPress={() => setShowIt(!showIt)}
-            >aperta ai
-            </Button>
-            {!!showIt ? <Text style={{ marginTop: 10, fontSize: 16 }}>{returnText}</Text> : null}
+        <View>
+            <Text style={styles.dayOfWeek}>{dayOfWeek}</Text>
+            <Picker
+                selectedValue={selectedSubject}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedSubject(itemValue)
+                }>
+
+                {subjectsOfWeek.map((subject) => {
+                    return (
+                        <Picker.Item label={subject.name} value={subject.name} />
+                    );
+                })}
+
+            </Picker>
         </View>
     );
 }
 
+export default function EditSemester() {
+
+    const subjectsMonday = subjects.monday.subjects;
+    const subjectsTuesday = subjects.tuesday.subjects;
+    const subjectsWednesday = subjects.wednesday.subjects;
+    const subjectsThursday = subjects.thursday.subjects;
+    const subjectsFriday = subjects.friday.subjects;
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
+    return (
+        <>
+            <TextInput
+                style={styles.inputSemesterName}
+                placeholder="Digite o semestre"
+                label="Digite o semestre"
+
+            ></TextInput>
+
+
+            <SelectSubjects dayOfWeek='Segunda-feira' subjectsOfWeek={subjectsMonday} />
+            <SelectSubjects dayOfWeek='Terça-feira' subjectsOfWeek={subjectsTuesday} />
+            <SelectSubjects dayOfWeek='Quarta-feira' subjectsOfWeek={subjectsWednesday} />
+            <SelectSubjects dayOfWeek='Quinta-feira' subjectsOfWeek={subjectsThursday} />
+            <SelectSubjects dayOfWeek='Sexta-feira' subjectsOfWeek={subjectsFriday} />
+
+            <View style={styles.currentSemester}>
+                <Text>Semestre Atual?</Text>
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+            </View>
+
+            <Button style={styles.btnSave} mode="contained" color="green">
+                Salvar Semestre
+            </Button>
+        </>
+    );
+}
+
+
+
 const styles = StyleSheet.create({
-    button: {
+
+    currentSemester: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+
+    },
+    inputSemesterName: {
+        height: 50,
+        fontSize: 18,
+        margin: 5,
+        lineHeight: 45,
+        marginBottom: 10
+    },
+    btnSave: {
         marginTop: 10,
         width: '50%',
         height: 40,
         alignSelf: 'center',
         justifyContent: 'center'
     },
-    basic: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+
+    dayOfWeek: {
+        fontSize: 18,
+        marginLeft: 10,
     }
 });
