74802ab1e52ba9a92e218fe7f00f8dc1a19119af
valentimkoniarski <valentim.koniarski@hotmail.com>
2021-11-27 14:21:01 -0300

    Save semester

--- screens/semester/EditSemester.js
+++ screens/semester/EditSemester.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, Text, TextInput, Switch } from "react-native-paper";
+import { Button, Text, TextInput, Switch, ScrollView } from "react-native-paper";
 
 import { Picker } from "@react-native-picker/picker";
 
@@ -10,8 +10,21 @@ import openDB from "../../db";
 
 import { useUsuario } from "../../context/UsuarioContext";
 
+const EMPTY_SEMESTER = {
+  usuario_id: "",
+  name: "",
+  materia1: "",
+  materia2: "",
+  materia3: "",
+  materia4: "",
+  materia5: "",
+  finalizarsemestre: "",
+  semestresatual: "",
+};
+
 const db = openDB();
 
+/* 
 function SelectSubjects({ subjectsOfWeek, dayOfWeek }) {
   const [selectedSubject, setSelectedSubject] = useState();
 
@@ -25,7 +38,8 @@ function SelectSubjects({ subjectsOfWeek, dayOfWeek }) {
       </Picker>
     </View>
   );
-}
+} 
+*/
 
 export default function EditSemester() {
   // agora que temos o UsuarioContext podemos
@@ -33,9 +47,48 @@ export default function EditSemester() {
   // e não precisar mais nos preocupar com recebe-lo
   // de um parametro vindo da navegação
   const { usuario } = useUsuario();
+  const [semester, setSemester] = useState({ ...EMPTY_SEMESTER });
+
 
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [returnText, setReturnText] = useState("");
+
+
+  function register() {
+
+    saveSemester(semester, semesterID => {
+
+      setSemester({ ...EMPTY_SEMESTER });
+
+      setReturnText("Semestre cadastrado com sucesso!");
+    });
+  }
+
+  function saveSemester(semester, onSuccessSaved) {
+
+    console.log(semester);
+
+    db.transaction(tx => {
+      tx.executeSql(
+        "INSERT INTO semestres (usuario_id, name, materia1, materia2, materia3, materia4, materia5, finalizarsemestre, semestresatual) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [usuario.id, semester.name, semester.materia1, semester.materia2, semester.materia3, semester.materia4, semester.materia5,
+        semester.finalizarsemestre, semester.semestresatual],
+        (_, rs) => {
+          console.log(`Semestre salvo com o ID: ${rs.insertId}`);
+          onSuccessSaved(rs.insertId);
+        },
+        (_, err) => {
+          console.log(err);
+        }
+
+      );
+    });
+  }
+
+
+  const [selectedSubject, setSelectedSubject] = useState();
 
   const subjectsMonday = subjects.monday.subjects;
   const subjectsTuesday = subjects.tuesday.subjects;
@@ -45,22 +98,74 @@ export default function EditSemester() {
 
   function recoverData() {
     db.transaction(tx => {
-      tx.executeSql("SELECT * FROM usuarios WHERE id = ?", [usuario.id], (_, rs) => {
-        setUsuarios(rs.rows._array);
-        setLoading(false);
+      tx.executeSql("SELECT * FROM semestres WHERE usuario_id = ?", [1], (_, rs) => {
+        //setUsuarios(rs.rows._array);
+        //setLoading(false);
+        console.log(rs.rows._array);
       });
     });
   }
 
   return (
     <>
-      <TextInput style={styles.inputSemesterName} placeholder="Digite o semestre" label="Digite o semestre"></TextInput>
+      <View>
+        <TextInput
+          style={styles.inputSemesterName}
+          placeholder="Digite o semestre"
+          label="Digite o semestre"
+          onChangeText={name => setSemester({ ...semester, name })}>
+        </TextInput>
+      </View>
+      <View>
+        <Text style={styles.dayOfWeek}>Segunda</Text>
+        <Picker selectedValue={selectedSubject} onValueChange={materia1 => setSemester({ ...semester, materia1 })}>
+          {subjectsMonday.map(subject => {
+            return <Picker.Item label={subject.name} value={subject.name} />;
+          })}
+        </Picker>
+      </View>
 
-      <SelectSubjects dayOfWeek="Segunda-feira" subjectsOfWeek={subjectsMonday} />
+      <View>
+        <Text style={styles.dayOfWeek}>Terca</Text>
+        <Picker selectedValue={selectedSubject} onValueChange={(itemValue, itemIndex) => setSelectedSubject(itemValue)}>
+          {subjectsTuesday.map(subject => {
+            return <Picker.Item label={subject.name} value={subject.name} />;
+          })}
+        </Picker>
+      </View>
+      <View>
+        <Text style={styles.dayOfWeek}>Quarta</Text>
+        <Picker selectedValue={selectedSubject} onValueChange={(itemValue, itemIndex) => setSelectedSubject(itemValue)}>
+          {subjectsWednesday.map(subject => {
+            return <Picker.Item label={subject.name} value={subject.name} />;
+          })}
+        </Picker>
+      </View>
+      <View>
+        <Text style={styles.dayOfWeek}>Quinta</Text>
+        <Picker selectedValue={selectedSubject} onValueChange={(itemValue, itemIndex) => setSelectedSubject(itemValue)}>
+          {subjectsThursday.map(subject => {
+            return <Picker.Item label={subject.name} value={subject.name} />;
+          })}
+        </Picker>
+      </View>
+      <View>
+        <Text style={styles.dayOfWeek}>Sexta</Text>
+        <Picker selectedValue={selectedSubject} onValueChange={(itemValue, itemIndex) => setSelectedSubject(itemValue)}>
+          {subjectsFriday.map(subject => {
+            return <Picker.Item label={subject.name} value={subject.name} />;
+          })}
+        </Picker>
+      </View>
+
+
+
+      {/*  
+     <SelectSubjects dayOfWeek="Segunda-feira" subjectsOfWeek={subjectsMonday} />
       <SelectSubjects dayOfWeek="Terça-feira" subjectsOfWeek={subjectsTuesday} />
       <SelectSubjects dayOfWeek="Quarta-feira" subjectsOfWeek={subjectsWednesday} />
       <SelectSubjects dayOfWeek="Quinta-feira" subjectsOfWeek={subjectsThursday} />
-      <SelectSubjects dayOfWeek="Sexta-feira" subjectsOfWeek={subjectsFriday} />
+      <SelectSubjects dayOfWeek="Sexta-feira" subjectsOfWeek={subjectsFriday} /> */}
 
       <View style={styles.currentSemester}>
         <Text>Semestre Atual?</Text>
@@ -73,9 +178,10 @@ export default function EditSemester() {
         />
       </View>
 
-      <Button style={styles.btnSave} mode="contained" color="green">
+      <Button style={styles.btnSave} mode="contained" color="green" onPress={() => recoverData()}>
         Salvar Semestre
       </Button>
+
     </>
   );
 }
--- screens/user/GridCollege.js
+++ screens/user/GridCollege.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Text, FlatList } from "react-native-paper";
+
 
 import { useUsuario } from "../../context/UsuarioContext";
 
@@ -12,17 +13,67 @@ export default function GridCollege() {
   const { usuario } = useUsuario();
   const [showIt, setShowIt] = useState(false);
 
+
+
+  const EMPTY_SEMESTER = {
+    usuario_id: "",
+    name: "",
+    materia1: "",
+    materia2: "",
+    materia3: "",
+    materia4: "",
+    materia5: "",
+    finalizarsemestre: "",
+    semestresatual: "",
+  };
+
+  const [semester, setSemester] = useState({ ...EMPTY_SEMESTER });
+
+
   const returnText = "pro player de react";
 
+
+  function recoverData() {
+    db.transaction(tx => {
+      tx.executeSql("SELECT * FROM semestres WHERE usuario_id = ?", [usuario.id], (_, rs) => {
+        setSemester(rs.rows._array);
+      });
+    });
+  }
+
+  console.log('dsad')
+  console.log(semester);
+
+
   return (
-    <View style={styles.basic}>
-      <Text>ID do Usuario: {usuario.id}</Text>
-      <Text>Nome do Usuário: {usuario.name}</Text>
-      <Button style={styles.button} mode="contained" color="green" onPress={() => setShowIt(!showIt)}>
-        aperta ai
-      </Button>
-      {!!showIt ? <Text style={{ marginTop: 10, fontSize: 16 }}>{returnText}</Text> : null}
-    </View>
+
+    <>
+      {/* <FlatList
+        renderItem={({ semester }) => (
+          <View style={styles.container}>
+            <Text>{semester.name}</Text>
+            <Text>{semester.materia1}</Text>
+            <Text>{semester.materia2}</Text>
+            <Text>{semester.materia3}</Text>
+            <Text>{semester.materia4}</Text>
+            <Text>{semester.materia5}</Text>
+            <Text>{semester.finalizarsemestre}</Text>
+          </View>
+        )}
+      >
+
+      </FlatList> */}
+
+      {/* <View style={styles.basic}>
+        <Text>ID do Usuario: {usuario.id}</Text>
+        <Text>Nome do Usuário: {usuario.name}</Text>
+        <Button style={styles.button} mode="contained" color="green" onPress={() => setShowIt(!showIt)}>
+          aperta ai
+        </Button>
+        {!!showIt ? <Text style={{ marginTop: 10, fontSize: 16 }}>{returnText}</Text> : null}
+      </View> */}
+    </>
+
   );
 }
 
